aab042c51654bcdf5268fb507c97ad9f32749ef2
Rishith Sundar <166918494+RishithSundar@users.noreply.github.com>
2025-03-24 12:40:14 +0530

    SSSPList.java

--- ADS Lab/SSSPList.java
+++ ADS Lab/SSSPList.java
@@ -1,14 +1,11 @@
 import java.util.*;
-
 public class SSSPListSimple {
     static void dijkstra(Map<Integer, List<int[]>> graph, int src, int n) {
         int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src] = 0;
-
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         pq.add(new int[]{src, 0});
-
         while (!pq.isEmpty()) {
             int[] curr = pq.poll();
             int u = curr[0];
@@ -20,17 +17,14 @@ public class SSSPListSimple {
                 }
             }
         }
-
         System.out.println(Arrays.toString(dist));
     }
-
     public static void main(String[] args) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
         graph.put(0, Arrays.asList(new int[]{1, 10}, new int[]{2, 20}));
         graph.put(1, Arrays.asList(new int[]{2, 5}, new int[]{3, 1}));
         graph.put(2, Arrays.asList(new int[]{3, 2}));
         graph.put(3, new ArrayList<>());
-
         dijkstra(graph, 0, 4);
     }
 }
