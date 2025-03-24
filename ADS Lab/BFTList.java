0971a288b5e8f39321942a44388c552f69d33ccb
Rishith Sundar <166918494+RishithSundar@users.noreply.github.com>
2025-03-24 12:15:05 +0530

    BFTList.java

---
+++ ADS Lab/BFTList.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class BFTList {
+    static void bfs(Map<Integer, List<Integer>> graph, int start) {
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        q.add(start);
+        visited.add(start);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            System.out.print(node + " ");
+            for (int neighbor : graph.get(node)) {
+                if (!visited.contains(neighbor)) {
+                    q.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(0, Arrays.asList(1, 2));
+        graph.put(1, Arrays.asList(0, 2, 3));
+        graph.put(2, Arrays.asList(0, 1, 3));
+        graph.put(3, Arrays.asList(1, 2));
+
+        System.out.print("BFT: ");
+        bfs(graph, 0);
+    }
+}
